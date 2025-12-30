b8b50396b7acd6b7c88ae11fce850a5ac5744d4f
Ricardo Santos <ricardo.batista.s@somoseducacao.com.br>
2025-12-30 15:52:42 -0300

    change structure directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
-import { HomeModule } from './home/home.module';
-import { ClientModule } from './person/client/src/client.module';
-import { SupplierModule } from './person/supplier/src/supplier.module';
-import { EmployeeModule } from './person/employee/src/employee.module';
-import { ProductModule } from './product/product/src/product.module';
-import { CategoryModule } from './product/category/src/category.module';
-import { SubCategoryModule } from './product/sub-category/src/sub-category.module';
-import { AccountsPayableModule } from './finance/accounts-payable/src/accounts-payable.module';
-import { AccountsReceivableModule } from './finance/accounts-receivable/src/accounts-receivable.module';
-import { BankAccountsModule } from './finance/bank-accounts/src/bank-accounts.module';
-import { BanksModule } from './finance/banks/src/banks.module';
-import { CashFlowModule } from './finance/cash-flow/src/cash-flow.module';
-import { PaymentMethodsModule } from './finance/payment-methods/src/payment-methods.module';
-import { TreasuryModule } from './finance/treasury/src/treasury.module';
-import { InventoryControlModule } from './inventory-control/src/inventory-control.module';
-import { TransporterModule } from './person/transporter/src/transporter.module';
-import { ProcurementModule } from './procurement/src/procurement.module';
-import { SalesModule } from './sales/src/sales.module';
-import { TaxManagementModule } from './tax-management/src/tax-management.module';
+import { HomeModule } from './modules/home/home.module';
+import { ClientModule } from './modules/person/client/src/client.module';
+import { SupplierModule } from './modules/person/supplier/src/supplier.module';
+import { EmployeeModule } from './modules/person/employee/src/employee.module';
+import { ProductModule } from './modules/product/product/src/product.module';
+import { CategoryModule } from './modules/product/category/src/category.module';
+import { SubCategoryModule } from './modules/product/sub-category/src/sub-category.module';
+import { AccountsPayableModule } from './modules/finance/accounts-payable/src/accounts-payable.module';
+import { AccountsReceivableModule } from './modules/finance/accounts-receivable/src/accounts-receivable.module';
+import { BankAccountsModule } from './modules/finance/bank-accounts/src/bank-accounts.module';
+import { BanksModule } from './modules/finance/banks/src/banks.module';
+import { CashFlowModule } from './modules/finance/cash-flow/src/cash-flow.module';
+import { PaymentMethodsModule } from './modules/finance/payment-methods/src/payment-methods.module';
+import { TreasuryModule } from './modules/finance/treasury/src/treasury.module';
+import { InventoryControlModule } from './modules/inventory-control/src/inventory-control.module';
+import { TransporterModule } from './modules/person/transporter/src/transporter.module';
+import { ProcurementModule } from './modules/procurement/src/procurement.module';
+import { SalesModule } from './modules/sales/src/sales.module';
+import { TaxManagementModule } from './modules/tax-management/src/tax-management.module';
 
 @Module({
   imports: [
