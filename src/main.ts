62b6291bf577ae4886fafb6d47c63293273aebdb
Ricardo Santos <ricardo.batista.s@somoseducacao.com.br>
2026-01-09 14:19:53 -0300

    change value static by enmviroment variable

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,5 @@ import { AppModule } from './app.module';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.APP_PORT);
 })()
